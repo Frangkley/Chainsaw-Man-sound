9f0e05d5c7e19bf17016568552230afea2b0aa53
Frangkley <frangkly@naver.com>
2023-01-09 12:20:00 +0900

    언어설정완료

--- Assets/MainEnKo.cs
+++ Assets/MainEnKo.cs
@@ -4,17 +4,61 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MainEnKo : MonoBehaviour
 {
+    public int 저장값 = 0;
     public Text 덴지버튼;
     public Text 마키마버튼;
+    public GameObject 왼쪽V표시;
+    public GameObject 오른쪽V표시;
+    public GameObject 언어창;
     // Start is called before the first frame update
     void Start()
     {
-        if (PlayerPrefs.GetInt("EnKo") != 0);
+        if (PlayerPrefs.GetInt("EnKo") != 0)
         {
             덴지버튼.text = "Denji";
+            저장값 = 1;
         }
     }
 
+    public void 한국어()
+    {
+        저장값 = 0;
+        왼쪽V표시.SetActive(true);
+        오른쪽V표시.SetActive(false);
+    }
+    public void 영어()
+    {
+        저장값 = 1;
+        왼쪽V표시.SetActive(false);
+        오른쪽V표시.SetActive(true);
+    }
+    public void OK()
+    {
+        if (저장값 == 0)
+        {
+            덴지버튼.text = "덴 지";
+        }
+        else
+        {
+            덴지버튼.text = "Denji";
+        }
+        PlayerPrefs.SetInt("EnKo", 저장값);
+        언어창.SetActive(false);
+    }
+    public void 언어창켜기()
+    {
+        언어창.SetActive(true);
+        if (저장값 == 0)
+        {
+            왼쪽V표시.SetActive(true);
+            오른쪽V표시.SetActive(false);
+        }
+        else
+        {
+            왼쪽V표시.SetActive(false);
+            오른쪽V표시.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
