67f2cdd8aa7b58597442869475efd0674c3d4286
Frangkley <frangkly@naver.com>
2023-01-31 09:48:02 +0900

    리세마라전

--- Assets/데이터관련스크립트/Le번역의요정개인.cs
+++ Assets/데이터관련스크립트/Le번역의요정개인.cs
@@ -39,6 +39,7 @@ public class Le번역의요정개인: MonoBehaviour
     public Sprite 아키이미지;
     public Sprite 마키마이미지;
     public Sprite 파워이미지;
+    public Sprite 히메노이미지;
     
     public void Awake()
     {
@@ -172,6 +173,9 @@ public class Le번역의요정개인: MonoBehaviour
                             break;
                         case "마키마":
                             일회용부모.transform.GetChild(1).gameObject.GetComponent<Image>().sprite = 마키마이미지;
+                            break;
+                        case "히메노":
+                            일회용부모.transform.GetChild(1).gameObject.GetComponent<Image>().sprite = 히메노이미지;
                             break;
                         default:
                             break;
